a1bbcdb8a800dec89c167de2d292475e58f63f80
JOSEPHNDAYISENGA <JOSEPH.NDAYISENG@GMAIL.COM>
2019-03-26 19:41:00 -0400

    new_add

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ let elTable = document.getElementById("store-table");
 let hoursArray = ["", "6:00 am", "7:00 am", "8:00 am", "9:00 am", "10:00 am", "11:00 am", "12:00 pm", "1:00 pm", "2:00 pm", "3:00 pm", "4:00 pm", "5:00 pm", "6:00 pm", "7:00 pm", "Daily location total"];
 
 
-// create a function that display the heading
+// create a function that display the table heading
 
 function displayHeading () {
     let elRowHeader = document.createElement("tr");
